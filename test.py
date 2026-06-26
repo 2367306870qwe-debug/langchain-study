bc0bfb2c9c7cee2355c16216d8b77395c87598de
lixuan001 <2367306870@qq.com>
2026-06-26 17:12:37 +0800

    5

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-print("Hello, World!")
-print("This is a test.")
-print("This is another test.")
-print("This is a fourth test.")
-print("This is a fifth test.")
-print("This is a sixth test.")
+import os
+# 正确导入DeepSeek
+from langchain_deepseek import ChatDeepSeek
+
+os.environ["DEEPSEEK_API_KEY"] = 'sk-2831e0e162d24451b7a8b63bd3d02821'
+
+# 初始化对话模型
+llm = ChatDeepSeek(
+    model="deepseek-chat",
+    max_tokens=1024
+)
+# invoke 标准调用方式，不能直接 llm(字符串)
+res = llm.invoke("介绍codex")
+print(res.content)
