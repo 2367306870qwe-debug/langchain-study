08e0b7a86ff7305e33a93d1e3b9563c15e27d508
lixuan001 <2367306870@qq.com>
2026-06-27 17:16:30 +0800

    2

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-1245
+from dotenv import load_dotenv
+load_dotenv()  
+
+from langchain.chat_models import init_chat_model
+import os
+base_url = os.getenv("DASHSCOPE_BASE_URL")
+api_key = os.getenv("DASHSCOPE_API_KEY")
+model = init_chat_model(
+    model="qwen3-omni-flash",
+    model_provider="openai",
+    base_url=base_url,
+    api_key=api_key
+)
+
+print(model.invoke("告诉我你现在使用的模型的完整名称"))
+
+
+
+
